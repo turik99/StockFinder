821e863e4ac883d2a4b9cfd555d2302876dd2f21
Eric <eric.zaba@gmail.com>
2017-07-22 20:40:46 -0500

    ....

--- app/src/main/java/com/ericz/stockfinder/MainActivity.java
+++ app/src/main/java/com/ericz/stockfinder/MainActivity.java
@@ -2,12 +2,37 @@ package com.ericz.stockfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Spinner percentSpinner;
+    private Spinner volumeSpinner;
+    private Spinner sectorSpinner;
+    private Spinner marketcapSpinner;
+    private Spinner peratioSpinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        percentSpinner = (Spinner) findViewById(R.id.percentSpinner);
+        volumeSpinner = (Spinner) findViewById(R.id.volumeSpinner);
+        sectorSpinner = (Spinner) findViewById(R.id.sectorSpinner);
+        marketcapSpinner = (Spinner) findViewById(R.id.marketcapSpinner);
+        peratioSpinner = (Spinner) findViewById(R.id.peratioSpinner);
     }
+
+    protected void stocks(View view)
+    {
+        String percentString = percentSpinner.getSelectedItem().toString();
+        String volumeString = volumeSpinner.getSelectedItem().toString();
+        String sectorString = sectorSpinner.getSelectedItem().toString();
+        String marketcapString = marketcapSpinner.getSelectedItem().toString();
+        String peratioString = peratioSpinner.getSelectedItem().toString();
+
+    }
+
+
 }
